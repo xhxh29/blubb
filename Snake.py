68eaf800025797fcf3e71a86ac494c7015045c38
M. Peter <mkp.peter@gmail.com>
2020-03-19 15:12:40 +0100

    Added movement class

---
+++ Snake.py
@@ -0,0 +1,11 @@
+class Snake:
+    def __init__(self, startpos, startdir):
+        self.position = [startpos]
+        self.direction = startdir
+    def move(self):
+        del self.position[-1]
+        self.position.insert(0, Head)
+    def __collision(self, colpos)
+    def __eat(self,foodpos)
+        
+
