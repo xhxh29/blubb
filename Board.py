b3ea85dc016abcb50cc7b3c1d5222cd79b571f51
Lukas Peter <lukaspeter61@gmail.com>
2020-03-19 16:21:25 +0100

    generate board content

--- Board.py
+++ Board.py
@@ -1,9 +1,30 @@
+import random
 class Board:
     def __init__(self, width, height, snake):
         self.width = width
         self.height = height
         self.snake = snake
+        self.board = [[BoardState.EMPTY for x in range(width)] for y in range(height)]
+        self.__gen_board(30)
     def collide(self, position):
+        if self.board[position[0]][position[1]] == BoardState.OBSTACLE:
+            return True
         return False
     def eat(self, position):
+        if self.board[position[0]][position[1]] == BoardState.FOOD:
+            self.board[position[0]][position[1]] = BoardState.EMPTY
+            return True
         return False
+    def __gen_board(self, density):
+        for i in range(self.width):
+            for j in range(self.height):
+                if random.randrange(density) == 0:
+                    self.board[i][j] = BoardState.FOOD
+                if i == 0 or j == 0 or i == self.width -1 or j == self.height -1:
+                    self.board[i][j] = BoardState.OBSTACLE
+
+from enum import Enum
+class BoardState(Enum):
+    EMPTY = 0
+    OBSTACLE = 1
+    FOOD = 2
--- MainTest.py
+++ MainTest.py
@@ -1,9 +1,16 @@
 import pygame
+from Board import Board
+from Snake import Snake
+from pygame import Rect
+from pygame import Color
 def main():
+    board = Board(20, 20, Snake((10,10),0))
     pygame.init()
-    screen = pygame.display.set_mode((100,100))
+    screen = pygame.display.set_mode((1000,1000))
     running = True
     while running:
+        pygame.draw.rect(screen, Color(255, 0 ,0),Rect(0,0,100,100))
+        pygame.display.update()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
