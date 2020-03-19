8c940457b054cab793745eb172a921b829efcdb8
Lukas Peter <lukaspeter61@gmail.com>
2020-03-19 13:40:34 +0100

    add sample window

--- MainTest.py
+++ MainTest.py
@@ -1,2 +1,10 @@
 import pygame
-print("Hello")
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((100,100))
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+main()
